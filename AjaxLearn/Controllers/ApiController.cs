153a1b701a55a19d2ca467750c7a5958c0004f8e
rick <chuechue777@gmail.com>
2024-01-29 19:46:22 +0800

    加入專案檔案。

---
+++ AjaxLearn/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using AjaxLearn.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxLearn.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly MyDBContext _context;
+        public ApiController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+            var cities = _context.Addresses.Select(s => s.City).Distinct();
+            return Json(cities);
+        }
+    }
+}
